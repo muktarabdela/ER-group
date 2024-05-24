28ddaaf6ecfdf6a012adeb9cb2b61099e2a0d898
muktar <muktarabdela10@gmail.com>
2024-05-24 08:26:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './component/Header'
+import Hero from './component/Hero'
+
+function App() {
+
+  return (
+    <div className=''>
+      <Header />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
